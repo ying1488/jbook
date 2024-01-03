8b5f182b154636b6436ee98db70d7f0a817c49ee
ying1488 <yingecteach@gmail.com>
2024-01-03 22:25:00 +1100

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(el!);
+
+const App = () => {
+
+  const [ input, setInput ] = useState('');
+  const [ code, setCode ] = useState('');
+
+  const startService = async () => {
+    try {
+      await esbuild.initialize({
+        worker: true,
+        wasmURL: '/esbuild.wasm'
+      });
+    } catch (err) {
+
+    }
+
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    try {
+      const res = await esbuild.build({
+        entryPoints: [ 'index.js' ],
+        bundle: true,
+        write: false,
+        plugins: [ unpkgPathPlugin() ]
+      });
+
+      // console.log(res);
+
+      setCode(res.outputFiles[ 0 ].text);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={e => setInput(e.target.value)}>
+      </textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+root.render(<App />);
