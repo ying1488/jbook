f05829e25ffeb706b60cf2fa20f46407110e42e2
ying1488 <yingecteach@gmail.com>
2024-01-08 22:51:33 +1100

    added Monaco code editor props of the editor updated

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor'
 
 const el = document.getElementById("root");
 
@@ -70,15 +71,18 @@ const App = () => {
       }
        },false);
        </script>
-    </body>s
+    </body>
   </html>
   `;
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
-        onChange={e => setInput(e.target.value)}>
+        onChange={(e) => {
+          setInput(e.target.value)
+        }}>
       </textarea>
       <div>
         <button onClick={onClick}>Submit</button>
