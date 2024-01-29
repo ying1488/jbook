2b150a045876c19650ef04c8eeb9426770706d6b
ying1488 <yingecteach@gmail.com>
2024-01-29 19:21:05 +1100

    extracted bundling logic from index.tsx to bundler

---
+++ src/bundler/index.ts
@@ -0,0 +1,32 @@
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
+import { fetchPlugin } from '../plugins/fetch-plugin';
+
+let service: boolean = false;
+
+const bundle = async (rawCode: string) => {
+  if (!service) {
+    await esbuild.initialize({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.19.11/esbuild.wasm'
+    });
+    service = true;
+  }
+
+  const result = await esbuild.build({
+    entryPoints: [ 'index.js' ],
+    bundle: true,
+    write: false,
+    plugins: [
+      unpkgPathPlugin(),
+      fetchPlugin(rawCode)
+    ],
+    define: {
+      'process.env.NODE_ENV': '"production"',
+      global: 'window',
+    },
+  })
+  return result.outputFiles[ 0 ].text
+};
+
+export default bundle;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
+import bundle from './bundler';
 
 const el = document.getElementById("root");
 
@@ -14,45 +12,10 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   const [ code, setCode ] = useState('');
   const [ input, setInput ] = useState('');
-  const startService = async () => {
-    try {
-      await esbuild.initialize({
-        worker: true,
-        wasmURL: 'https://unpkg.com/esbuild-wasm@0.19.11/esbuild.wasm'
-      });
-    } catch (err) {
-
-    }
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    try {
-
-      const result = await esbuild.build({
-        entryPoints: [ 'index.js' ],
-        bundle: true,
-        write: false,
-        plugins: [
-          unpkgPathPlugin(),
-          fetchPlugin(input)
-        ],
-        define: {
-          'process.env.NODE_ENV': '"production"',
-          global: 'window',
-        },
-      });
-      setCode(result.outputFiles[ 0 ].text);
-
-    } catch (err) {
-      console.error(err);
-    }
+    const output = await bundle(input);
+    setCode(output);
   };
-
-
 
   return (
     <div>
@@ -61,20 +24,12 @@ const App = () => {
         onChange={(value) => setInput(value)
         }
       />
-      <textarea
-        value={input}
-        onChange={(e) => {
-          setInput(e.target.value)
-        }}>
-      </textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-
       <Preview code={code} />
     </div>
   );
 };
-
 
 root.render(<App />);
