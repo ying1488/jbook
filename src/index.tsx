2ac2750c6e1e6e7b3ad71c2a75f8730240f66dc0
ying1488 <yingecteach@gmail.com>
2024-01-29 18:41:51 +1100

    refactored iframe from index to preview component

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,19 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-
 import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
-import CodeEditor from './components/code-editor'
+import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
-
-  const iframe = useRef<any>();
+  const [ code, setCode ] = useState('');
   const [ input, setInput ] = useState('');
-
-
   const startService = async () => {
     try {
       await esbuild.initialize({
@@ -35,8 +32,6 @@ const App = () => {
   const onClick = async () => {
     try {
 
-      iframe.current.srcdoc = html;
-
       const result = await esbuild.build({
         entryPoints: [ 'index.js' ],
         bundle: true,
@@ -50,32 +45,14 @@ const App = () => {
           global: 'window',
         },
       });
-      // setCode(result.outputFiles[ 0 ].text);
-      iframe.current.contentWindow.postMessage(result.outputFiles[ 0 ].text, '*');
+      setCode(result.outputFiles[ 0 ].text);
+
     } catch (err) {
       console.error(err);
     }
   };
 
-  const html = `
-  <html>
-    <head></head>
-    <body>
-     <div id="root"></div>
-       <script>
-       window,addEventListener('message',(event)=>{
-      try{
-        eval(event.data)
-      } catch(err){
-        const root = document.querySelector('#root');
-        root.innerHTML ='<div style="color: red;"><h4>Runtime Error</h4>' +err +'</div>';
-        console.error(err);
-      }
-       },false);
-       </script>
-    </body>
-  </html>
-  `;
+
 
   return (
     <div>
@@ -94,7 +71,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
 
-      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+      <Preview code={code} />
     </div>
   );
 };
