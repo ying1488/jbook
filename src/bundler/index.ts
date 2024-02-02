bf2c61c37e7596447282045da43777dcd55d1ba3
ying1488 <yingecteach@gmail.com>
2024-02-02 16:11:20 +1100

    error handling for inpur of code in the CodeCell

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,5 +1,5 @@
 import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 import { fetchPlugin } from './plugins/fetch-plugin';
 
 let service: boolean = false;
@@ -13,20 +13,35 @@ const bundle = async (rawCode: string) => {
     service = true;
   }
 
-  const result = await esbuild.build({
-    entryPoints: [ 'index.js' ],
-    bundle: true,
-    write: false,
-    plugins: [
-      unpkgPathPlugin(),
-      fetchPlugin(rawCode)
-    ],
-    define: {
-      'process.env.NODE_ENV': '"production"',
-      global: 'window',
-    },
-  })
-  return result.outputFiles[ 0 ].text
+  try {
+    const result = await esbuild.build({
+      entryPoints: [ 'index.js' ],
+      bundle: true,
+      write: false,
+      plugins: [
+        unpkgPathPlugin(),
+        fetchPlugin(rawCode)
+      ],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+    return {
+      code: result.outputFiles[ 0 ].text,
+      err: ''
+    }
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      return {
+        code: '',
+        err: err.message,
+      };
+    } else {
+      throw err;
+    }
+  };
 };
 
 export default bundle;
